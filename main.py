a014527d7df67374318b0d92806d33907dddd380
lucas2510290431003-dev <lucas.2510290431003@aluno.cvttrs.faetec.rj.gov.br>
2026-07-15 20:02:35 -0300

    Sistema Bithelp Finalizado

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ except:
 # --- 1. CONFIGURAÇÃO DA PÁGINA ---
 st.set_page_config(
     page_title="Bithelp - GearTech Solutions",
-    page_icon="bithelplogo.png",
+    page_icon="bithelp_icon.png",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
